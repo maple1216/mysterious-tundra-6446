1a532b2c81bbfcf2f6a623cd004985b01ba13ca4
Joseph Milla <jjtmilla@gmail.com>
2015-02-21 11:47:45 -0600

    Set up Angular framework
    Let's get started, shall we? 😉

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# mysterious-tundra-6446
+# mysterious-tundra6646
 
-So mysterious. Ugh.
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.11.1.
 
-💩
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mysteriousTundra6646App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mysteriousTundra6646App
+ */
+angular.module('mysteriousTundra6646App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
