5a39b5925cefd44a7cd29c3e708cd9e21bd8df0d
Joseph Milla <jjtmilla@gmail.com>
2015-02-22 15:56:50 -0600

    Add scripts for app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,22 @@
  *
  * Main module of the application.
  */
-angular
-  .module('mysteriousTundra6446App', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('mytodoApp', [
+'ngCookies',
+'ngResource',
+'ngSanitize',
+'ngRoute',
+'ui.sortable',
+'LocalStorageModule'
+])
+.config(['localStorageServiceProvider', '$routeProvider', function(localStorageServiceProvider, $routeProvider){
+ localStorageServiceProvider.setPrefix('ls');
+ $routeProvider
+   .when('/', {
+     templateUrl: 'views/main.html',
+     controller: 'MainCtrl'
+   })
+   .otherwise({
+     redirectTo: '/'
+   });
+}]);
