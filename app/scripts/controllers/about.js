2cb45598a096344cb2c1e574de237a6ae7c06220
Joseph Milla <jjtmilla@gmail.com>
2015-02-22 15:54:20 -0600

    Reset yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mysteriousTundra6446App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mysteriousTundra6446App
+ */
+angular.module('mysteriousTundra6446App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mysteriousTundra6446App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mysteriousTundra6446App
+ */
+angular.module('mysteriousTundra6446App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
