7d203febc4d7b887cee20bb0cbe1e195d92e1d81
Joseph Milla <jjtmilla@gmail.com>
2015-02-22 15:57:37 -0600

    Add main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the mysteriousTundra6446App
  */
-angular.module('mysteriousTundra6446App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+angular.module('mytodoApp')
+ .controller('MainCtrl', function ($scope, localStorageService) {
+
+   var todosInStore = localStorageService.get('todos');
+
+   $scope.todos = todosInStore || [];
+
+   $scope.$watch('todos', function () {
+     localStorageService.add('todos', $scope.todos);
+   }, true);
+
+   // Uncomment if you are disabling persistence
+   //$scope.todos = [];
+
+   $scope.addTodo = function () {
+     $scope.todos.push($scope.todo);
+     $scope.todo = '';
+   };
+
+   $scope.removeTodo = function (index) {
+     $scope.todos.splice(index, 1);
+   };
+
+ });
